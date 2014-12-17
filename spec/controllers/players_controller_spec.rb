5a54e613f587920e2779e0a21bf3c51500390efd
Daniel Koza <dakolech@gmail.com>
2014-12-17 10:52:33 +0100

    add players controller

---
+++ spec/controllers/players_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe PlayersController, :type => :controller do
+
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
