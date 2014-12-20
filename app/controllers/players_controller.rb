e6368584c786368c34004ce00e2d38d908d87e5a
Daniel Koza <dakolech@gmail.com>
2014-12-20 17:01:53 +0100

    update player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,22 +1,46 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
   end
 
   def show
-  end
-
-  def create
+    @player = Player.find(params[:id])
   end
 
   def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+
+    if @player.save
+      redirect_to @player, notice: 'Player was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def edit
+    @player = Player.find(params[:id])
   end
 
   def update
+    @player = Player.find(params[:id])
+
+    if @player.update(player_params)
+      redirect_to @player, notice: 'Player was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @player.destroy
   end
+
+  private
+    def player_params
+      params.require(:player).permit(:firstname, :lastname, :email, :phone_number, :max_break, :date_of_birth, :city)
+    end
 end
