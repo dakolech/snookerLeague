ef707b435240b3ee53ee42b56baef81483169d56
Daniel Koza <dakolech@gmail.com>
2014-12-22 21:30:01 +0100

    add leagues controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   resources :players
+
+  resources :leagues do
+    patch :add_player
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
