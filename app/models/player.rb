10dea7ea9cde3ada7ef5fddde81db885c8543035
Daniel Koza <dakolech@gmail.com>
2015-02-21 15:34:15 +0100

    add database schema png

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,9 @@
 class Player < ActiveRecord::Base
   has_and_belongs_to_many :leagues, :join_table => 'leagues_players'
-  #has_many :matches
   has_many :homematches, :class_name => 'Match', :foreign_key => 'player_1_id'
   has_many :awaymatches, :class_name => 'Match', :foreign_key => 'player_2_id'
   has_many :homeframes, :class_name => 'Frame', :foreign_key => 'player_1_id'
   has_many :awayframes, :class_name => 'Frame', :foreign_key => 'player_2_id'
-  #has_many :frames
   has_many :breaks
 
   validates :firstname, presence: true
